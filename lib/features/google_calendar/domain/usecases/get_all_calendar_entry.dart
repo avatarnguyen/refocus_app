5f1ec645459aaa336f5b454b1083f7c8cbd3b8eb
Anh Nguyen <nguyenanh@hotmail.de>
2021-07-20 00:45:42 +0200

    add interface for usecase

--- lib/features/google_calendar/domain/usecases/get_all_calendar_entry.dart
+++ lib/features/google_calendar/domain/usecases/get_all_calendar_entry.dart
@@ -1,16 +1,17 @@
 import 'package:dartz/dartz.dart';
-import 'package:refocus_app/core/error/failures.dart';
-import 'package:refocus_app/features/google_calendar/domain/entities/google_calendar_entry.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/google_calendar_entry.dart';
 import '../repositories/google_calendar_repository.dart';
 
-class GetAllCalendarEntry {
+class GetAllCalendarEntry implements UseCase<GoogleCalendarEntry, NoParams> {
   GetAllCalendarEntry(this.repository);
 
   final GoogleCalendarRepository repository;
 
-  Future<Either<Failure, GoogleCalendarEntry>> execute(
-      {required String message}) async {
+  @override
+  Future<Either<Failure, GoogleCalendarEntry>> call(NoParams params) async {
     return await repository.getAllCalendarEntries();
   }
 }
--- test/features/google_calendar/domain/usecases/get_all_calendar_entry_test.dart
+++ test/features/google_calendar/domain/usecases/get_all_calendar_entry_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:refocus_app/core/usecases/usecase.dart';
 import 'package:refocus_app/features/google_calendar/domain/entities/google_calendar_entry.dart';
 import 'package:refocus_app/features/google_calendar/domain/repositories/google_calendar_repository.dart';
 import 'package:refocus_app/features/google_calendar/domain/usecases/get_all_calendar_entry.dart';
@@ -18,7 +19,7 @@ void main() {
     usecase = GetAllCalendarEntry(mockGoogleCalendarRepository);
   });
 
-  final tEntry = 'Test';
+  // final tEntry = 'Test';
   final tGoogleCalendarEntry = GoogleCalendarEntry(summary: 'Test');
 
   test(
@@ -28,7 +29,7 @@ void main() {
       when(() => mockGoogleCalendarRepository.getAllCalendarEntries())
           .thenAnswer((_) async => Right(tGoogleCalendarEntry));
       // act
-      final result = await usecase.execute(message: tEntry);
+      final result = await usecase(NoParams());
       // assert
       expect(result, Right(tGoogleCalendarEntry));
       verify(mockGoogleCalendarRepository.getAllCalendarEntries);
